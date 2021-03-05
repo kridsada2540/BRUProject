48b46f4d8f68b2314c91c75c2752173f7256f383
kridsada2540 <promphanat@gmail.com>
2021-03-05 07:37:48 +0700

    add

--- bru_center/models/type_product.py
+++ bru_center/models/type_product.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 
 class TypeProduct(models.Model):
     _name = 'bru.type'
+    _rec_name = 'type_product'
     _description = 'Type Product'
 
     type_product = fields.Char(
